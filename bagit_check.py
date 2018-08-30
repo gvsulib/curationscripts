9188a9765b1f7764b46b8634f34f0d20076037f1
Kyle Felker <felkerk@gvsu.edu>
2018-08-30 18:24:01 +0000

    adding bagit bag verification

--- bagit_check.py
+++ bagit_check.py
@@ -3,14 +3,21 @@
 #aws python library
 import boto3
 
+import bagit
+
+import subprocess
+
 #not sure why, but you seem to have to import this to get proper error handling for boto3
 import botocore
 import os
 
 #neccessary for easy file/directory deletions
 import shutil
-import datetime
+
+#used for sorting versions of files by date
 from collections import OrderedDict
+
+#regular expression functions, for parsing files and file paths
 import re
 
 #function to figure out which version of a file is the most recent undeleted version
@@ -19,6 +26,8 @@ def latestVersionID (key, bucket):
         versions = list(bucket.object_versions.filter(Prefix=key))
 
         timestamps = {}
+
+	#are there no versions for this file?  If so, return false
 	if len(versions) < 1:
 		return False
 
@@ -30,19 +39,21 @@ def latestVersionID (key, bucket):
                         timestamps[version.id] = version.last_modified
         #if we make it this far, the latest version is deleted, and we need to find the most recent undeleted version
         orderedTimeStamp = OrderedDict(sorted(timestamps.items(), key=lambda x: x[1]))
-        try:
-		first = orderedTimeStamp.popitem()
-        except KeyError as e:
-		print("keyerror with: " + key)  	
+        
+	first = orderedTimeStamp.popitem()
 	return first[0]
 
 #check to see if local directory for files exists, and scrub it if it does
 if os.path.exists("bagit_verify"):
 	shutil.rmtree("bagit_verify")
 
-
+#now recreate it
 os.makedirs("bagit_verify")
 	
+if os.path.exists("bagit_verify.log"):
+	os.remove("bagit_verify.log")
+
+logfile = open("bagit_verify.log", "w")
 
 #alphaList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
@@ -53,10 +64,10 @@ s3 = boto3.resource('s3')
 
 bucket = s3.Bucket('scholarworkslifecycle')
 
+
 #start constructing the local bags
 for alpha in alphaList:
-
-	
+	path = "bagit_verify/sw-" + alpha
 
 	keyList = ["manifest-sha256.txt",
 			"manifest-sha512.txt",
@@ -64,7 +75,6 @@ for alpha in alphaList:
 			"tagmanifest-sha512.txt",
 			"bagit.txt",
 			"bag-info.txt" ]
-	path = "bagit_verify/sw-" + alpha
 	os.makedirs(path)	
 	for key in keyList:
 		try:
@@ -77,10 +87,9 @@ for alpha in alphaList:
         			raise e
 	
 
-	
+	#open the manifest file and extract all the file keys
 	manifest = open(path + "/manifest-sha512.txt", "r")
 	lines = manifest.readlines()
-	#extract and construct the key from the manifest
 	files = []
 	for line in lines:
 		extract = re.split(r'\s', line)
@@ -90,6 +99,9 @@ for alpha in alphaList:
 		files.append(key)
 	
 	manifest.close()
+
+	#now start downloading all the files in the manifest, checking for ones that have been deleted and getting the most recent 
+	#undeleted versions instead
 	for key in files:
 	
 		#extract the filename for downloading
@@ -99,7 +111,7 @@ for alpha in alphaList:
 		#get the rest of the file path so we can make sure the directory structure exists
 		file_path = re.search(r'/.*/', key)
 		file_path = file_path.group(0)						
-		
+		#if the directory structure doesn't exist, create it.
 		if not os.path.exists(path + file_path):
 			os.makedirs(path + file_path)
 
@@ -108,8 +120,36 @@ for alpha in alphaList:
 		idnum = latestVersionID(key, bucket)
 		if idnum == False:
 			print("Cound not find any versions of file: " + key)
-			continue
+			
 		bucket.download_file(key, path + "/" + filename, ExtraArgs={'VersionId': idnum})
+	
+
+
+        try:
+                bag = bagit.Bag(path)
+        except bagit.BagError as e:
+                print("error accessing bag" + path)
+                logfile.write("error accessing bag: " + path)
+                logfile.write(str(e))
+                continue
+
+        isError = False
+
+
+        try:
+                bag.validate()
+        except bagit.BagValidationError as e:
+
+                logfile.write("problem validating bag " + path)
+                logfile.write(str(e))
+
+                isError = True
+
+        if isError:
+                print("Problem verifying bag " + path + " Check bagit_verify.log for further details")
+        else:
+                print("Bag " + path + "Downloaded and successfully verified, moving on to next bag.")
+		
 	
 		
 		
